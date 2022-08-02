56976f0962325d7a4938b9d93d3f75db9cbf4b61
ghugh8 <ghugh87@gmail.com>
2022-08-02 20:35:55 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 module.exports = function towelSort (matrix) {
-  if (matrix.length == 0) {
+  if (matrix && matrix.length == 0) {
       return [];
-    } else if (matrix.length == 2) {
+    } else if (matrix && matrix.length == 2) {
       const arr = matrix[1].reverse();
       const b = matrix[0].concat(...arr);
       return b;
-    } else if (matrix.length == 3) {
+    } else if (matrix && matrix.length == 3) {
       const arr = matrix[1].reverse();
       const b = matrix[0].concat(...arr, matrix[2]);
       return b.flat();
-    } else if (matrix.length == 4) {
+    } else if (matrix && matrix.length == 4) {
       const arr = [matrix[0], matrix[1].reverse(), matrix[2], matrix[3].reverse()];
       arrConcat = Array.prototype.concat(...arr);
       return arrConcat;
-    } if (matrix) {
+    } else {
       return [];
     }
 }
